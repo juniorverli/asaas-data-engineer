cae5c69ad00d4b693d8a57642077ce1dc287fd23
Dionei Junior <dionei.junior@outlook.com>
2022-10-24 00:58:21 -0300

    atualizacao

--- python/main.py
+++ python/main.py
@@ -1,103 +1,157 @@
+from time import sleep
+from pyspark.sql.session import SparkSession
+import hashlib
 import requests
 import json
+import pyspark.sql.functions as F
+from schemas import *
 import mysql.connector
-import hashlib
 
-#Keys necessárias para a API e Banco de Dados
-TIMESTAMP = '1'
 PRIVATE_KEY = ''
 PUBLIC_KEY = ''
-COMBINED_KEY = TIMESTAMP+PRIVATE_KEY+PUBLIC_KEY
-MD5HASH = hashlib.md5(COMBINED_KEY.encode()).hexdigest()
-MYSQL_USER = ''
-MYSQL_PASSWORD_ROOT = ''
+TIMESTAMP = '10'
+COMBINED_KEYS = TIMESTAMP+ PRIVATE_KEY + PUBLIC_KEY
+MD5HASH = hashlib.md5(COMBINED_KEYS.encode()).hexdigest()
 
 
-#Conectando ao banco de dados
-cnx = mysql.connector.connect(
-    host="db", user=MYSQL_USER, password=MYSQL_PASSWORD_ROOT, database="db", port="3306")
-cursor = cnx.cursor()
+CONECTOR_TYPE = "com.mysql.cj.jdbc.Driver"
+MYSQL_USERNAME = "root"
+MYSQL_PASSWORD = "root"
+MYSQL_DBNAME = "db"
+MYSQL_SERVERNAME = "db_MYSQL"
+MYSQL_PORT = "3306"
+url_mysql = f'jdbc:mysql://{MYSQL_SERVERNAME}:{MYSQL_PORT}/{MYSQL_DBNAME}'
 
-#Função de conexão aos endpoints para retornar o json
 def get_api_marvel(session, endpoint, offset):
+
+    sleep(2)
     url = f'https://gateway.marvel.com:443/v1/public/{endpoint}?ts={TIMESTAMP}&apikey={PUBLIC_KEY}&hash={MD5HASH}&offset={offset}&limit=100'
-    r = session.get(url)
-    return r.json()
+    try:
+        r = session.get(url)
+        return r.json()
+    except ConnectionError as e:
+        print("CONNECTION ERROR: ")
+        print(e)
+        error = True
+        return error
 
-#Função específica para tratar a data de modificação, pois em alguns casos algumas datas estavam vindo fora do padrão
-def get_datemodified(i):
-    if len(i['modified'][:-5]) != 19:
-        datemodified = None
-    else:
-        datemodified = i['modified'][:-5].replace("T", " ")
-    return datemodified
+def write_json(endpoint, get_endpoint):
+    with open(f'/usr/app/src/files/{endpoint}_data.json', 'w') as outfile:
+        json.dump(get_endpoint['data']['results'], outfile)
 
-#Função criada para a extração dos dados
-def extraction(endpoint):
+def read_json_with_spark(endpoint, schema):
+    result = spark.read.schema(schema).json(f'/usr/app/src/files/{endpoint}_data.json', multiLine = "true")
+    return result
 
-    #Para que não perca nenhum dado foi criado a tabela config no banco de dados onde é retornado o valor do offset que será utilizado na quantidade de retornos dentro da API
-    sql = f"SELECT * FROM config WHERE name = '{endpoint}'"
-    cursor.execute(sql)
+def write_mysql(endpoint, rawDF, mode):
+    rawDF.write \
+        .format("jdbc") \
+        .option("url", url_mysql) \
+        .option("driver", CONECTOR_TYPE) \
+        .option("dbtable", endpoint) \
+        .option("user", MYSQL_USERNAME) \
+        .option("password", MYSQL_PASSWORD) \
+        .mode(mode) \
+        .save()
+
+def read_with_spark(endpoint):
+    result = spark.read \
+        .format("jdbc") \
+        .option("url", url_mysql) \
+        .option("driver", CONECTOR_TYPE) \
+        .option("dbtable", endpoint) \
+        .option("user", MYSQL_USERNAME) \
+        .option("password", MYSQL_PASSWORD) \
+        .load()
+    return result
+
+def extraction(endpoint, schema):
+
+    sql_config = f"SELECT * FROM config WHERE name = '{endpoint}'"
+    cursor.execute(sql_config)
     offset = cursor.fetchone()
-    #Retorna o valor de offset
     offset = int(offset[1])
+
     s = requests.Session()
-    #Retorna o valor de quantos characters e comics tem dentro da base da API
     get_endpoint = get_api_marvel(s, endpoint, offset)
+    while get_endpoint is True:
+         get_endpoint = get_api_marvel(s, endpoint, offset)
+
     total = get_endpoint['data']['total']
 
     while offset < total:
-        #Aqui executa linha por linha a inserção dos dados de cada tabela
-        if endpoint == 'characters':
-            for i in get_endpoint['data']['results']:
 
-                for x in i['comics']['items']:
-                    sql = "INSERT INTO characters (id, name, description, modified, resourceURI, thumbnail, comicId) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                    datemodified = get_datemodified(i)
-                    val = (i['id'], i['name'], i['description'], datemodified, i['resourceURI'], i['thumbnail']['path'], x['resourceURI'][43:])
+        write_json(endpoint, get_endpoint)
+        rawDF = read_json_with_spark(endpoint, schema)
 
-                    cursor.execute(sql, val)
+        if endpoint == 'comics':
 
-                offset = offset+1
-                update_offset = f"UPDATE config SET offset = {offset} WHERE name = 'characters'"
-                cursor.execute(update_offset)
-                #O commit só é realizado no final da query pois é ele que irá confirmar que não perdemos nenhum dado
-                cnx.commit()
-        elif endpoint == 'comics':
-            for i in get_endpoint['data']['results']:
-                sql = "INSERT INTO comics (id, digitalid, title, issueNumber, variantDescription, description, modified, isbn, upc, \
-                    diamondCode, ean, issn, format, pageCount, resourceURI, thumbnail) \
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-                datemodified = get_datemodified(i)
-                val = (i['id'], i['digitalId'], i['title'], i['issueNumber'], i['variantDescription'], i['description'], datemodified, \
-                    i['isbn'], i['upc'], i['diamondCode'], i['ean'], i['issn'], i['format'], i['pageCount'], i['resourceURI'], i['thumbnail']['path']) 
+            rawDF = rawDF.withColumn("thumbnail", F.col("thumbnail.path"))\
+                .withColumn("prices", F.explode_outer("prices"))\
+                .withColumn("prices", F.col("prices.price"))\
+                .withColumnRenamed("prices", "price")\
+                .withColumn("modified", F.to_date("modified"))\
+                .filter(rawDF.title.isNotNull())
 
-                cursor.execute(sql, val)
-                offset = offset+1
-                update_offset = f"UPDATE config SET offset = {offset} WHERE name = 'comics'"
-                cursor.execute(update_offset)
-                #O commit só é realizado no final da query pois é ele que irá confirmar que não perdemos nenhum dado
-                cnx.commit()
-        s = requests.Session()
+        elif endpoint == 'characters':
+
+            rawDF = rawDF.withColumn("comics", F.arrays_zip("comics.items"))\
+                .withColumn("comics", F.explode_outer("comics.items"))\
+                .withColumn("comics", F.col("comics.resourceURI"))\
+                .withColumn("comics", F.expr("substring(comics, 44, length(comics)-43)"))\
+                .withColumnRenamed("comics", "comicId")\
+                .withColumn("thumbnail", F.col("thumbnail.path"))\
+                .withColumn("modified", F.to_date("modified"))
+    
+        write_mysql(endpoint, rawDF, "append")
+
+        offset += 100
+        update_offset = f"UPDATE config SET offset = {offset} WHERE name = '{endpoint}'"
+        cursor.execute(update_offset)
+        cnx.commit()
+
         get_endpoint = get_api_marvel(s, endpoint, offset)
 
+def prefix_columns(endpoint):
+    for column in endpoint.columns:
+        endpoint = endpoint.withColumnRenamed(column, endpoint + "_" + column)
+    return endpoint
 
 def main():
 
-    extraction('characters')
-    extraction('comics')
+    extraction('comics', comicsSchema)
+    extraction('characters', charactersSchema)
 
-    #Após executado a extração é criado um script que confirma se existe a OBT e caso exista é realizado o DROP da tabela para criação da mesma
-    sql = "DROP TABLE IF EXISTS db.all_characters_and_comics; \
-        CREATE TABLE db.all_characters_and_comics \
-        SELECT cha.id as characterId, cha.name as character_name, \
-        cha.description as character_description, cha.modified as character_modified, \
-        cha.resourceURI as character_resourceURI, cha.thumbnail as character_thumbnail, \
-        co.id as comicId, co.digitalId as comic_digitalId, co.title as comic_title, co.issueNumber as comic_issueNumber, \
-        co.variantDescription as comic_variantDescription, co.description as comic_description, co.modified as comic_modified, \
-        co.isbn as comic_isbn, co.upc as comic_upc, co.diamondCode as comic_diamondCode, co.ean as comic_ean, co.issn as comic_issn, \
-        co.format as comic_format, co.pageCount as comic_pageCount, co.resourceURI as comic_resourceURI, co.thumbnail as comic_thumbnail FROM db.characters as cha \
-        INNER JOIN db.comics as co on cha.comicId = co.id;"
+    comics = read_with_spark('comics')
+    for column in comics.columns:
+        comics = comics.withColumnRenamed(column, "comics" + "_" + column)
+
+    characters = read_with_spark('characters')
+    for column in characters.columns:
+        characters = characters.withColumnRenamed(column, "characters" + "_" + column)
+
+    obtDF = characters.join(comics, characters.characters_comicId == comics.comics_id, how = 'inner') \
+        .drop("characters_comicId") \
+        .dropDuplicates()
+
+    write_mysql('all_comics_and_characters', obtDF, "overwrite")
+
+if __name__ == '__main__':
+
+    spark = SparkSession.builder \
+            .appName("PySpark") \
+            .master("local[*]") \
+            .config("spark.driver.extraClassPath", "/usr/app/src/java/mysql-connector-java-8.0.22.jar") \
+            .getOrCreate()
+
+    cnx = mysql.connector.connect(
+    host=MYSQL_SERVERNAME, user=MYSQL_USERNAME, password=MYSQL_PASSWORD, database=MYSQL_DBNAME, port=MYSQL_PORT)
+    cursor = cnx.cursor()
+
+    sql = "CREATE TABLE IF NOT EXISTS config \
+        SELECT name, offset FROM (VALUES ROW('comics', 0), \
+        ROW('characters', 0) ) As config (name, offset)"
+    
     cursor.execute(sql)
 
-main()
+    main()
